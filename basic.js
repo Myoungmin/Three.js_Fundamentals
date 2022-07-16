2f240946dcd62689407d138cb196a76f2f2021e0
Myoungmin <miung222@gmail.com>
2022-07-16 22:50:25 +0900

    basic.js 추가, App 클래스 생성자 정의

---
+++ basic.js
@@ -0,0 +1,52 @@
+import * as Three from './three.module.js';
+
+class App {
+    constructor() {
+        // id가 webgl-container인 div요소를 얻어와서, 상수에 저장 
+        const divContainer = document.querySelector("#webgl-container");
+        // 얻어온 상수를 클래스 필드에 정의
+        // 다른 메서드에서 참조할 수 있도록 필드에 정의한다.
+        this._divContainer = divContainer;
+
+        // 렌더러 생성, Three.js의 WebGLRenderer 클래스로 생성
+        // antialias를 활성화 시키면 렌더링될 때 오브젝트들의 경계선이 계단 현상 없이 부드럽게 표현된다.
+        const renderer = new Three.WebGLRenderer({ antialias: true });
+        // window의 devicePixelRatio 속성을 얻어와 PixelRatio 설정
+        // 디스플레이 설정의 배율값을 얻어온다.
+        renderer.setPixelRatio(window.devicePixelRatio);
+        // domElement를 자식으로 추가.
+        // canvas 타입의 DOM 객체이다.
+        // 문서 객체 모델(DOM, Document Object Model)은 XML이나 HTML 문서에 접근하기 위한 일종의 인터페이스.
+        divContainer.appendChild(renderer.domElement);
+        // 다른 메서드에서 참조할 수 있도록 필드에 정의한다.
+        this._renderer = renderer;
+
+        // Scene 객체 생성
+        const scene = new Three.Scene();
+        // 다른 메서드에서 참조할 수 있도록 필드에 정의한다.
+        this._scene = scene;
+
+        // 카메라 객체를 구성
+        this._setupCamera();
+        // 조명 설정
+        this._setupLight();
+        // 3D 모델 설정
+        this._setupModel();
+
+        // 창 크기가 변경될 때 발생하는 이벤트인 onresize에 App 클래스의 resize 메서드를 연결한다.
+        // this가 가리키는 객체가 이벤트 객체가 아닌 App클래스 객체가 되도록 하기 위해 bind로 설정한다.
+        // onresize 이벤트가 필요한 이유는 렌더러와 카메라는 창 크기가 변경될 때마다 그 크기에 맞게 속성값을 재설정해줘야 한다.
+        window.onresize = this.resize.bind(this);
+        // onresize 이벤트와 상관없이 생성자에서 resize 메서드를 호출한다.
+        // 렌더러와 카메라의 속성을 창크기에 맞게 설정해준다. 
+        this.resize();
+
+        // render 메서드를 requestAnimationFrame이라는 API에 넘겨줘서 호출해준다.
+        // render 메서드 안에서 쓰이는 this가 App 클래스 객체를 가리키도록 하기 위해 bind 사용
+        requestAnimationFrame(this.render.bind(this));
+    }
+}
+
+window.onload = function () {
+    new App();
+}
